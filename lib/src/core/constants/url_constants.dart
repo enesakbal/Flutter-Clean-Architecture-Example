ed3bd1919218694154875b0ae19968cef103fffa
Enes Akbal <enesakbal00@gmail.com>
2023-10-07 00:43:54 +0300

    chore: added url constants

---
+++ lib/src/core/constants/url_constants.dart
@@ -0,0 +1,9 @@
+class UrlConstants {
+  //* Movie
+  static const popularMovies = '/movie/popular';
+  static const topRatedMovies = '/movie/top_rated';
+
+  //* Actor
+  static const actorDetail = '/person/{person_id}';
+  static const actorSocialMedia = '/person/{person_id}/external_ids';
+}
