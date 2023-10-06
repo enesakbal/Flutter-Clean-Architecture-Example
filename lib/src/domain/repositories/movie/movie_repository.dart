81d83a1e274e316f3ebb815492b95d216d50b3ef
Enes Akbal <enesakbal00@gmail.com>
2023-10-06 23:59:09 +0300

    feat: added new local methods to movie repository

--- lib/src/domain/repositories/movie/movie_repository.dart
+++ lib/src/domain/repositories/movie/movie_repository.dart
@@ -1,10 +1,22 @@
 import 'package:dartz/dartz.dart';
 
-import '../../../core/network/network_exception.dart';
+import '../../../core/exceptions/database/database_exception.dart';
+import '../../../core/exceptions/network/network_exception.dart';
+import '../../entities/movie_detail/movie_detail_entity.dart';
 import '../../entities/movie_listings/movie_listings_entity.dart';
 
 abstract class MovieRepository {
-  Future<Either<NetworkExceptions, MovieListingsEntity>> getTopRatedMovies({required int page});
+  //* Remote Data Source
+  Future<Either<NetworkException, MovieListingsEntity>> getTopRatedMovies({required int page});
 
-  Future<Either<NetworkExceptions, MovieListingsEntity>> getPopularMovies({required int page});
+  Future<Either<NetworkException, MovieListingsEntity>> getPopularMovies({required int page});
+
+  //* Local Data Source
+  Future<Either<DatabaseException, List<MovieDetailEntity>>> getSavedMovieDetails();
+
+  Future<Either<DatabaseException, void>> saveMovieDetails({required MovieDetailEntity movieDetailEntity});
+
+  Future<Either<DatabaseException, void>> deleteMovieDetail({required int? movieId});
+
+  Future<Either<DatabaseException, bool>> isSavedMovieDetail({required int? movieId});
 }
--- lib/src/domain/usecases/movie/movie_usecases.dart
+++ lib/src/domain/usecases/movie/movie_usecases.dart
@@ -1,6 +1,8 @@
 import 'package:dartz/dartz.dart';
 
-import '../../../core/network/network_exception.dart';
+import '../../../core/exceptions/database/database_exception.dart';
+import '../../../core/exceptions/network/network_exception.dart';
+import '../../entities/movie_detail/movie_detail_entity.dart';
 import '../../entities/movie_listings/movie_listings_entity.dart';
 import '../../repositories/movie/movie_repository.dart';
 
@@ -9,9 +11,34 @@ class MovieUsecases {
 
   const MovieUsecases(this._movieRepository);
 
-  Future<Either<NetworkExceptions, MovieListingsEntity>> getPopularMovies({required int page}) async =>
+  //* REMOTE
+  /// This method gets popular movies from the remote data source.
+  Future<Either<NetworkException, MovieListingsEntity>> getPopularMovies({required int page}) async =>
       _movieRepository.getPopularMovies(page: page);
 
-  Future<Either<NetworkExceptions, MovieListingsEntity>> getTopRatedMovies({required int page}) async =>
+  /// This method gets top rated movies from the remote data source.
+  Future<Either<NetworkException, MovieListingsEntity>> getTopRatedMovies({required int page}) async =>
       _movieRepository.getTopRatedMovies(page: page);
+
+  //* LOCAL
+  /// This method gets saved movie details from the local data source.
+  Future<Either<DatabaseException, void>> getSavedMovieDetails() async => _movieRepository.getSavedMovieDetails();
+
+  /// This method toggles bookmark for a movie in the local data source.
+  Future<Either<DatabaseException, void>> toggleBookmark({required MovieDetailEntity movieDetailEntity}) async {
+    final isSaved = await _movieRepository.isSavedMovieDetail(movieId: movieDetailEntity.id);
+
+    return isSaved.fold(
+      (error) {
+        return Left(error);
+      },
+      (isSaved) {
+        if (isSaved) {
+          return _movieRepository.deleteMovieDetail(movieId: movieDetailEntity.id);
+        } else {
+          return _movieRepository.saveMovieDetails(movieDetailEntity: movieDetailEntity);
+        }
+      },
+    );
+  }
 }
