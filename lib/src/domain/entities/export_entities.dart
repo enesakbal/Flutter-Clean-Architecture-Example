59e1b559ee7c7cbe55162a7d19d24bc8baebbaa4
Enes Akbal <enesakbal00@gmail.com>
2023-10-07 01:28:55 +0300

    chore: exported entities

---
+++ lib/src/domain/entities/export_entities.dart
@@ -0,0 +1,4 @@
+export 'actor_detail/actor_detail_entity.dart';
+export 'actor_social_media/actor_social_medias_entity.dart';
+export 'movie_detail/movie_detail_entity.dart';
+export 'movie_listings/movie_listings_entity.dart';
