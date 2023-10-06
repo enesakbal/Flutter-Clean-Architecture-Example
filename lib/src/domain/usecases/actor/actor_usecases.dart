5f4eb89a613af083421fcb6e2c420f74048f05f1
Enes Akbal <enesakbal00@gmail.com>
2023-10-06 23:58:12 +0300

    chore: rename changes

--- lib/src/domain/usecases/actor/actor_usecases.dart
+++ lib/src/domain/usecases/actor/actor_usecases.dart
@@ -1,6 +1,6 @@
 import 'package:dartz/dartz.dart';
 
-import '../../../core/network/network_exception.dart';
+import '../../../core/exceptions/network/network_exception.dart';
 import '../../entities/actor_detail/actor_detail_entity.dart';
 import '../../entities/actor_social_media/actor_social_medias_entity.dart';
 import '../../repositories/actor/actor_repository.dart';
@@ -10,9 +10,11 @@ class ActorUsecases {
 
   const ActorUsecases(this._actorRepository);
 
-  Future<Either<NetworkExceptions, ActorDetailEntity>> getActorDetail({required String actorId}) async =>
+  /// This method gets actor detail from the remote data source.
+  Future<Either<NetworkException, ActorDetailEntity>> getActorDetail({required String actorId}) async =>
       _actorRepository.getActorDetail(actorId: actorId);
 
-  Future<Either<NetworkExceptions, ActorSocialMediaEntity>> getActorSocialMedia({required String actorId}) async =>
+  /// This method gets actor social media from the remote data source.
+  Future<Either<NetworkException, ActorSocialMediaEntity>> getActorSocialMedia({required String actorId}) async =>
       _actorRepository.getActorSocialMedia(actorId: actorId);
 }
