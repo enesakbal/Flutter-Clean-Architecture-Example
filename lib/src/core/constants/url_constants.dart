7643ea3eb342c5945d1d9e78c57d541b6d3995ca
Enes <enesakbal00@gmail.com>
2023-11-26 18:29:51 +0300

    chore: added movie cretedit end point

--- lib/src/core/constants/url_constants.dart
+++ lib/src/core/constants/url_constants.dart
@@ -2,6 +2,7 @@ class UrlConstants {
   //* Movie
   static const popularMovies = '/movie/popular';
   static const topRatedMovies = '/movie/top_rated';
+  static const movieCredits = '/movie/{movie_id}/credits';
 
   //* Actor
   static const actorDetail = '/person/{person_id}';
