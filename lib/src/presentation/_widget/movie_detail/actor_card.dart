860c2e75c8e5b96f8ae2c1a1a981bc5d6b6bfbc1
Enes <enesakbal00@gmail.com>
2024-01-02 23:42:50 +0300

    feat: actor card

---
+++ lib/src/presentation/_widget/movie_detail/actor_card.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../core/components/image/base_network_image.dart';
+import '../../../core/constants/image_constants.dart';
+import '../../../domain/entities/export_entities.dart';
+
+class ActorCard extends StatelessWidget {
+  const ActorCard({
+    super.key,
+    required this.castEntity,
+  });
+
+  final CastEntity? castEntity;
+
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12.r),
+        color: Theme.of(context).primaryColorDark,
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(12.r),
+              bottomLeft: Radius.circular(12.r),
+            ),
+            child: SizedBox(
+              width: 65.w,
+              child: BaseNetworkImage(
+                ImageConstants.getOriginalImagePath(posterPath: castEntity?.profilePath),
+                hasRadius: false,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.w),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '${castEntity?.name}',
+                    maxLines: 2,
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                  5.verticalSpace,
+                  Text(
+                    '${castEntity?.character}',
+                    maxLines: 1,
+                    style: Theme.of(context).textTheme.labelMedium,
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
