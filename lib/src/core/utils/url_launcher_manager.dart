77a3917aad20d754c9b64ca59d8f4407b1364d48
Enes <enesakbal00@gmail.com>
2024-01-06 00:26:50 +0300

    feat: url launcher manager

---
+++ lib/src/core/utils/url_launcher_manager.dart
@@ -0,0 +1,73 @@
+import 'dart:developer';
+
+import 'package:url_launcher/url_launcher_string.dart';
+
+class UrlLauncherManager {
+  static Future<void> redirectUrl(String url) async {
+    if (await canLaunchUrlString(url)) {
+      await launchUrlString(url);
+    } else {
+      log('Could not launch $url');
+    }
+  }
+
+  static Future<void> redirectToInstagramById(String id) async {
+    final url = 'https://www.instagram.com/$id';
+
+    if (await canLaunchUrlString(url)) {
+      await launchUrlString(url);
+    } else {
+      log('Could not launch $url');
+    }
+  }
+
+  static Future<void> redirectToTwitterById(String id) async {
+    final url = 'https://twitter.com/$id';
+
+    if (await canLaunchUrlString(url)) {
+      await launchUrlString(url);
+    } else {
+      log('Could not launch $url');
+    }
+  }
+
+  static Future<void> redirectToFacebookById(String id) async {
+    final url = 'https://www.facebook.com/$id';
+
+    if (await canLaunchUrlString(url)) {
+      await launchUrlString(url);
+    } else {
+      log('Could not launch $url');
+    }
+  }
+
+  static Future<void> redirectToYoutubeById(String id) async {
+    final url = 'https://www.youtube.com/channel/$id';
+
+    if (await canLaunchUrlString(url)) {
+      await launchUrlString(url);
+    } else {
+      log('Could not launch $url');
+    }
+  }
+
+  static Future<void> redirectToImdbById(String id) async {
+    final url = 'https://www.imdb.com/name/$id';
+
+    if (await canLaunchUrlString(url)) {
+      await launchUrlString(url);
+    } else {
+      log('Could not launch $url');
+    }
+  }
+
+  static Future<void> redirectToTiktokById(String id) async {
+    final url = 'https://www.tiktok.com/@$id';
+
+    if (await canLaunchUrlString(url)) {
+      await launchUrlString(url);
+    } else {
+      log('Could not launch $url');
+    }
+  }
+}
