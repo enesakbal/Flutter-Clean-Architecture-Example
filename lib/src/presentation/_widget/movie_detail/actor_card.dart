5c5de3367620b76f45a477cd7ce76aeb22514096
Enes <enesakbal00@gmail.com>
2024-01-06 18:27:19 +0300

    feat: added social media bottom sheet to actor card

--- lib/src/presentation/_widget/movie_detail/actor_card.dart
+++ lib/src/presentation/_widget/movie_detail/actor_card.dart
@@ -3,6 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../core/components/image/base_network_image.dart';
 import '../../../domain/entities/export_entities.dart';
+import 'social_media_bottom_sheet.dart';
 
 class ActorCard extends StatelessWidget {
   const ActorCard({
@@ -14,50 +15,54 @@ class ActorCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DecoratedBox(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(12.r),
-        color: Theme.of(context).disabledColor,
-      ),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(12.r),
-              bottomLeft: Radius.circular(12.r),
-            ),
-            child: SizedBox(
-              width: 65.w,
-              child: BaseNetworkImage.originalImageSize(
-                castEntity?.profilePath,
-                hasRadius: false,
+    return InkWell(
+      onTap: () async => SocialMediaBottomSheet(actorId: castEntity?.id?.toString()).show(context),
+      borderRadius: BorderRadius.circular(12.r),
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12.r),
+          color: Theme.of(context).disabledColor,
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(12.r),
+                bottomLeft: Radius.circular(12.r),
+              ),
+              child: SizedBox(
+                width: 65.w,
+                child: BaseNetworkImage.originalImageSize(
+                  castEntity?.profilePath,
+                  hasRadius: false,
+                ),
               ),
             ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16.w),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    '${castEntity?.name}',
-                    maxLines: 2,
-                    style: Theme.of(context).textTheme.titleMedium,
-                  ),
-                  5.verticalSpace,
-                  Text(
-                    '${castEntity?.character}',
-                    maxLines: 1,
-                    style: Theme.of(context).textTheme.labelMedium,
-                  ),
-                ],
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.w),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '${castEntity?.name}',
+                      maxLines: 2,
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                    5.verticalSpace,
+                    Text(
+                      '${castEntity?.character}',
+                      maxLines: 1,
+                      style: Theme.of(context).textTheme.labelMedium,
+                    ),
+                  ],
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
