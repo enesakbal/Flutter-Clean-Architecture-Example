2c6124c8cacda8f0321a1fd0715532c7cd3629e8
Enes <enesakbal00@gmail.com>
2024-01-13 22:41:50 +0300

    ref: removed _openDb func from MovieLocalDataSourceImpl added db parameter for db operations

--- lib/src/data/datasources/local/movie/movie_local_data_source_impl.dart
+++ lib/src/data/datasources/local/movie/movie_local_data_source_impl.dart
@@ -1,27 +1,18 @@
 import 'package:isar/isar.dart';
-import 'package:path_provider/path_provider.dart';
 
 import '../_collections/movie_detail/movie_detail_collection.dart';
 import 'movie_local_data_source.dart';
 
 class MovieLocalDataSourceImpl implements MovieLocalDataSource {
-  const MovieLocalDataSourceImpl();
+  const MovieLocalDataSourceImpl(this.db);
 
-  /// Opens the Isar database and returns a [Future] of [Isar] instance.
-  Future<Isar> _openDb() async {
-    final directory = await getApplicationDocumentsDirectory();
-    return Isar.open([MovieDetailCollectionSchema], directory: directory.path);
-  }
+  final Isar db;
 
   /// Deletes the movie detail with the given [movieId] from the local database.
   @override
   Future<void> deleteMovieDetail({required int? movieId}) async {
     try {
-      final db = await _openDb();
-
-      db.writeTxn(() async => db.movieDetailCollections.filter().idEqualTo(movieId).deleteAll());
-
-      await db.close();
+      await db.writeTxn(() async => db.movieDetailCollections.filter().idEqualTo(movieId).deleteAll());
     } catch (_) {
       rethrow;
     }
@@ -31,11 +22,7 @@ class MovieLocalDataSourceImpl implements MovieLocalDataSource {
   @override
   Future<List<MovieDetailCollection>> getSavedMovieDetails() async {
     try {
-      final db = await _openDb();
-
       final list = await db.movieDetailCollections.where().findAll();
-
-      await db.close();
 
       return list;
     } catch (_) {
@@ -47,10 +34,7 @@ class MovieLocalDataSourceImpl implements MovieLocalDataSource {
   @override
   Future<void> saveMovieDetail({required MovieDetailCollection movieDetailCollection}) async {
     try {
-      final db = await _openDb();
-      db.writeTxn(() async => db.movieDetailCollections.put(movieDetailCollection));
-
-      await db.close();
+      await db.writeTxn(() async => db.movieDetailCollections.put(movieDetailCollection));
     } catch (_) {
       rethrow;
     }
@@ -60,11 +44,7 @@ class MovieLocalDataSourceImpl implements MovieLocalDataSource {
   @override
   Future<bool> isSavedMovieDetail({required int? movieId}) async {
     try {
-      final db = await _openDb();
-
       final isSaved = await db.movieDetailCollections.filter().idEqualTo(movieId).isNotEmpty();
-
-      await db.close();
 
       return isSaved;
     } catch (_) {
