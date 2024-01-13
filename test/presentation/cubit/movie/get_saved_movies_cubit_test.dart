e296dbd2b6a2889ae76a98af3539c59791525060
Enes <enesakbal00@gmail.com>
2024-01-14 01:11:49 +0300

    test: unit tests for GetSavedMoviesCubit

---
+++ test/presentation/cubit/movie/get_saved_movies_cubit_test.dart
@@ -0,0 +1,105 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_clean_architecture/src/core/exceptions/database/database_exception.dart';
+import 'package:flutter_clean_architecture/src/domain/entities/export_entities.dart';
+import 'package:flutter_clean_architecture/src/domain/usecases/export_usecases.dart';
+import 'package:flutter_clean_architecture/src/presentation/cubit/movie/export_movie_cubits.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:isar/isar.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../../_utils/mocks/mocks.mocks.dart';
+
+void main() {
+  late final MovieUsecases movieUsecases;
+
+  late final MovieDetailEntity tMovieDetailEntity1;
+  late final MovieDetailEntity tMovieDetailEntity2;
+  late final MovieDetailEntity tMovieDetailEntity3;
+  late final MovieDetailEntity tMovieDetailEntity4;
+
+  setUpAll(() {
+    movieUsecases = MockMovieUsecases();
+
+    tMovieDetailEntity1 = const MovieDetailEntity(
+      id: 1,
+      title: 'title',
+      overview: 'overview',
+      posterPath: 'posterPath',
+      backdropPath: 'backdropPath',
+    );
+
+    tMovieDetailEntity2 = const MovieDetailEntity(
+      id: 2,
+      title: 'title',
+      overview: 'overview',
+      posterPath: 'posterPath',
+      backdropPath: 'backdropPath',
+    );
+
+    tMovieDetailEntity3 = const MovieDetailEntity(
+      id: 3,
+      title: 'title',
+      overview: 'overview',
+      posterPath: 'posterPath',
+      backdropPath: 'backdropPath',
+    );
+
+    tMovieDetailEntity4 = const MovieDetailEntity(
+      id: 4,
+      title: 'title',
+      overview: 'overview',
+      posterPath: 'posterPath',
+      backdropPath: 'backdropPath',
+    );
+  });
+
+  blocTest<GetSavedMoviesCubit, GetSavedMoviesState>(
+    'should emit [GetSavedMoviesLoading, GetSavedMoviesLoaded] when success',
+    setUp: () {
+      final tMovieList = [
+        tMovieDetailEntity1,
+        tMovieDetailEntity2,
+        tMovieDetailEntity3,
+        tMovieDetailEntity4,
+      ];
+
+      provideDummy<Either<DatabaseException, List<MovieDetailEntity>>>(Right(tMovieList));
+
+      when(movieUsecases.getSavedMovieDetails()).thenAnswer((_) async => Right(tMovieList));
+    },
+    build: () => GetSavedMoviesCubit(movieUsecases),
+    act: (bloc) => bloc.getSavedMovieDetails(),
+    expect: () => [
+      const GetSavedMoviesLoading(),
+      GetSavedMoviesLoaded(
+        movies: [
+          tMovieDetailEntity1,
+          tMovieDetailEntity2,
+          tMovieDetailEntity3,
+          tMovieDetailEntity4,
+        ],
+      ),
+    ],
+    verify: (_) => verify(movieUsecases.getSavedMovieDetails()).called(1),
+  );
+
+  blocTest<GetSavedMoviesCubit, GetSavedMoviesState>(
+    'should emit [GetSavedMoviesLoading, GetSavedMoviesError] when failure',
+    setUp: () {
+      final isarError = IsarError('isarError');
+
+      provideDummy<Either<DatabaseException, List<MovieDetailEntity>>>(Left(DatabaseException.fromIsarError(isarError)));
+
+      when(movieUsecases.getSavedMovieDetails())
+          .thenAnswer((_) async => Left(DatabaseException.fromIsarError(isarError)));
+    },
+    build: () => GetSavedMoviesCubit(movieUsecases),
+    act: (bloc) => bloc.getSavedMovieDetails(),
+    expect: () => [
+      const GetSavedMoviesLoading(),
+      const GetSavedMoviesError(message: 'isarError'),
+    ],
+    verify: (_) => verify(movieUsecases.getSavedMovieDetails()).called(1),
+  );
+}
