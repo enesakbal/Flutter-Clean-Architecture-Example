9b4d5568c9a7743025337db8364bbe1d048b5b9f
Enes <enesakbal00@gmail.com>
2024-01-14 16:15:34 +0300

    test: actor cubit tests

---
+++ test/presentation/cubit/actor/get_actor_detail_cubit_test.dart
@@ -0,0 +1,70 @@
+import 'dart:io';
+
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_clean_architecture/src/core/exceptions/network/network_exception.dart';
+import 'package:flutter_clean_architecture/src/domain/entities/export_entities.dart';
+import 'package:flutter_clean_architecture/src/domain/usecases/export_usecases.dart';
+import 'package:flutter_clean_architecture/src/presentation/cubit/actor/export_actor_cubits.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../../_utils/mocks/mocks.mocks.dart';
+
+void main() {
+  late final ActorUsecases actorUsecases;
+
+  late final ActorDetailEntity tActorDetailEntity;
+
+  setUpAll(() {
+    actorUsecases = MockActorUsecases();
+
+    tActorDetailEntity = const ActorDetailEntity(
+      id: 1,
+      name: 'name',
+      biography: 'biography',
+      birthday: 'birthday',
+      deathday: 'deathday',
+      placeOfBirth: 'placeOfBirth',
+      profilePath: 'profilePath',
+    );
+  });
+
+  blocTest<GetActorDetailCubit, GetActorDetailState>(
+    'should emit [GetActorDetailLoading, GetActorDetailLoaded] when success',
+    build: () {
+      provideDummy<Either<NetworkException, ActorDetailEntity>>(Right(tActorDetailEntity));
+
+      when(actorUsecases.getActorDetail(actorId: '1')).thenAnswer((_) async => Right(tActorDetailEntity));
+
+      return GetActorDetailCubit(actorUsecases);
+    },
+    act: (bloc) => bloc.getActorDetail(actorId: '1'),
+    expect: () => [const GetActorDetailLoading(), GetActorDetailLoaded(actor: tActorDetailEntity)],
+    verify: (_) => verify(actorUsecases.getActorDetail(actorId: '1')).called(1),
+  );
+
+  blocTest<GetActorDetailCubit, GetActorDetailState>(
+    'should emit [GetActorDetailLoading, GetActorDetailError] when internet connection error occurs with SocketException',
+    build: () {
+      final dioException = DioException(
+        requestOptions: RequestOptions(),
+        error: const SocketException(''),
+        type: DioExceptionType.connectionError,
+      );
+      provideDummy<Either<NetworkException, ActorDetailEntity>>(Left(NetworkException.fromDioError(dioException)));
+
+      when(actorUsecases.getActorDetail(actorId: '1'))
+          .thenAnswer((_) async => Left(NetworkException.fromDioError(dioException)));
+
+      return GetActorDetailCubit(actorUsecases);
+    },
+    act: (bloc) => bloc.getActorDetail(actorId: '1'),
+    expect: () => [
+      const GetActorDetailLoading(),
+      const GetActorDetailError(message: 'Please check your internet connection'),
+    ],
+    verify: (_) => verify(actorUsecases.getActorDetail(actorId: '1')).called(1),
+  );
+}
---
+++ test/presentation/cubit/actor/get_actor_social_media_test.dart
@@ -0,0 +1,67 @@
+import 'dart:io';
+
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_clean_architecture/src/core/exceptions/network/network_exception.dart';
+import 'package:flutter_clean_architecture/src/domain/entities/export_entities.dart';
+import 'package:flutter_clean_architecture/src/domain/usecases/export_usecases.dart';
+import 'package:flutter_clean_architecture/src/presentation/cubit/actor/export_actor_cubits.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../../_utils/mocks/mocks.mocks.dart';
+
+void main() {
+  late final ActorUsecases actorUsecases;
+
+  late final ActorSocialMediaEntity tActorSocialMediaEntity;
+
+  setUpAll(() {
+    actorUsecases = MockActorUsecases();
+
+    tActorSocialMediaEntity = const ActorSocialMediaEntity(
+      id: 1,
+      facebookId: 'facebookId',
+      instagramId: 'instagramId',
+      twitterId: 'twitterId',
+    );
+  });
+
+  blocTest<GetActorSocialMediaCubit, GetActorSocialMediaState>(
+    'should emit [GetActorSocialMediaLoading, GetActorSocialMediaLoaded] when success',
+    build: () {
+      provideDummy<Either<NetworkException, ActorSocialMediaEntity>>(Right(tActorSocialMediaEntity));
+
+      when(actorUsecases.getActorSocialMedia(actorId: '1')).thenAnswer((_) async => Right(tActorSocialMediaEntity));
+
+      return GetActorSocialMediaCubit(actorUsecases);
+    },
+    act: (bloc) => bloc.getActorSocialMedia(actorId: '1'),
+    expect: () => [const GetActorSocialMediaLoading(), GetActorSocialMediaLoaded(data: tActorSocialMediaEntity)],
+    verify: (_) => verify(actorUsecases.getActorSocialMedia(actorId: '1')).called(1),
+  );
+
+  blocTest<GetActorSocialMediaCubit, GetActorSocialMediaState>(
+    'should emit [GetActorSocialMediaLoading, GetActorSocialMediaError] when internet connection error occurs with SocketException',
+    build: () {
+      final dioException = DioException(
+        requestOptions: RequestOptions(),
+        error: const SocketException(''),
+        type: DioExceptionType.connectionError,
+      );
+      provideDummy<Either<NetworkException, ActorDetailEntity>>(Left(NetworkException.fromDioError(dioException)));
+
+      when(actorUsecases.getActorSocialMedia(actorId: '1'))
+          .thenAnswer((_) async => Left(NetworkException.fromDioError(dioException)));
+
+      return GetActorSocialMediaCubit(actorUsecases);
+    },
+    act: (bloc) => bloc.getActorSocialMedia(actorId: '1'),
+    expect: () => [
+      const GetActorSocialMediaLoading(),
+      const GetActorSocialMediaError(message: 'Please check your internet connection'),
+    ],
+    verify: (_) => verify(actorUsecases.getActorSocialMedia(actorId: '1')).called(1),
+  );
+}
